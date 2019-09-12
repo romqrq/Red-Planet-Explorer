17ce87995cac0f10531777bf9223202b0b189f99
Ubuntu <ubuntu@ip-172-31-11-161.eu-west-1.compute.internal>
2019-09-12 12:44:56 +0000

    Latest images API request sorted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,77 @@
-var roverName = "curiosity";
-var solNumber = "" //"&sol=1000";
-var camName = "&camera=FHAZ";
-var pageNumber = "" //"&page=1"
-var earthDate = "earth_date=2015-6-3"
+// var roverName = "curiosity";
+// var solNumber = ""; //"&sol=1000";
+// var camName = "&camera=FHAZ";
+// var pageNumber = ""; //"&page=1"
+// var earthDate = "earth_date=2015-6-3"
+
 
 //WORKS xhr.open("GET", "https://api.nasa.gov/insight_weather/?api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b&feedtype=json&ver=1.0");
 //WORKS xhr.open("GET", "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b&feedtype=json&ver=1.0");
 //WORKS xhr.open("GET", "https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b&feedtype=json&ver=1.0");
 
-function getData(key, cb) {
-    var xhr = new XMLHttpRequest();
-    
-    xhr.open("GET", `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?${earthDate}${solNumber}${camName}${pageNumber}&api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b`);
-    xhr.send();
 
 
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-            // console.log(typeof(this.responseText));
-        }
-    };
+
+//Pick latest photo by rover name
+
+function setName(namePicked, cb) {
+	var roverName = namePicked;
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/latest_photos?api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b&feedtype=json&ver=1.0`);
+	xhr.send();
+
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(JSON.parse(this.responseText));
+			cb(JSON.parse(this.responseText));
+		}
+	};
 }
 
-function writeToDocument (key) {
-	getData(key, function(data){
-		console.dir(data);
-		// document.getElementById("data").innerHTML = data;
+function writeToData2() {
+	var el = document.getElementById("data");
+	el.innerHTML = "";
+
+	getLatest(function(data) {
+		data = data.latest_photos;
+
+		data.forEach(function(item) {
+			el.innerHTML += `<img src=${item.img_src} height=150 width=150>`;
+		});
+	});
+}
+
+
+
+
+
+
+
+function getLatest(cb) {
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			// document.getElementById("data").innerHTML = JSON.parse(this.responseText);
+			console.log(JSON.parse(this.responseText));
+			cb(JSON.parse(this.responseText));
+		}
+	};
+
+	xhr.open("GET", "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b&feedtype=json&ver=1.0");
+	xhr.send();
+}
+
+function writeToDocument() {
+	var el = document.getElementById("data");
+	el.innerHTML = "";
+
+	getLatest(function(data) {
+		data = data.latest_photos;
+
+		data.forEach(function(item) {
+			el.innerHTML += `<img src=${item.img_src} height=150 width=150>`;
+		});
 	});
 }
