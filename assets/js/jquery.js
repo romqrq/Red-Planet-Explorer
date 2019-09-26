590f7f584626083638e3e19d19e026e573f4f890
Ubuntu <ubuntu@ip-172-31-11-161.eu-west-1.compute.internal>
2019-09-26 18:20:51 +0000

    General commit

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
         var elTarget = document.getElementById("weather-section");
         elTarget.scrollIntoView();
     }
+// console.log(("#data2").children("img"));
+//     $("#data2").children("img").click(function() {
+//         var imgID = $(this).attr("id");
+//         console.log(imgID);
+//         var galModal = document.getElementById("galModal");
+//         // galModal.innerHTML = "";
+//         galModal.innerHTML = `
+//         <div id="myModal" class="modal">
+//         <span class="close">&times;</span>
+//         <img class="modal-content" id="${imgID}">`;
+//     });
 });
