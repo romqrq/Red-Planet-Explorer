78493b7a45235b2d3d36ea781b30f60fe982fe86
romqrq <rjaalbuquerque@gmail.com>
2019-10-01 17:31:25 +0100

    Fixed modal video autoplay

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+    autoPlayYouTubeModal();
+
     $("#launch-button").click(function() {
         $("header").addClass("shake-animation");
         $("#launch-button").removeClass("launch-button-animation");
@@ -9,15 +12,30 @@ $(document).ready(function() {
         var elTarget = document.getElementById("weather-section");
         elTarget.scrollIntoView();
     }
-// console.log(("#data2").children("img"));
-//     $("#data2").children("img").click(function() {
-//         var imgID = $(this).attr("id");
-//         console.log(imgID);
-//         var galModal = document.getElementById("galModal");
-//         // galModal.innerHTML = "";
-//         galModal.innerHTML = `
-//         <div id="myModal" class="modal">
-//         <span class="close">&times;</span>
-//         <img class="modal-content" id="${imgID}">`;
-//     });
+    // console.log(("#data2").children("img"));
+    //     $("#data2").children("img").click(function() {
+    //         var imgID = $(this).attr("id");
+    //         console.log(imgID);
+    //         var galModal = document.getElementById("galModal");
+    //         // galModal.innerHTML = "";
+    //         galModal.innerHTML = `
+    //         <div id="myModal" class="modal">
+    //         <span class="close">&times;</span>
+    //         <img class="modal-content" id="${imgID}">`;
+    //     });
+
+    //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+    //code from https://stackoverflow.com/questions/18622508/bootstrap-3-and-youtube-in-modal
+    function autoPlayYouTubeModal() {
+        var trigger = $("body").find('[data-toggle="modal"]');
+        trigger.click(function() {
+            var theModal = $(this).data("target"),
+                videoSRC = $(this).attr("data-theVideo"),
+                videoSRCauto = videoSRC + "?autoplay=1";
+            $(theModal + ' iframe').attr('src', videoSRCauto);
+            $(theModal + ' button.close').click(function() {
+                $(theModal + ' iframe').attr('src', videoSRC);
+            });
+        });
+    }
 });
