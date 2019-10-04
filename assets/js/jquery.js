3be7ce92c2073717fcb68848a92122db4ebba579
romqrq <rjaalbuquerque@gmail.com>
2019-10-04 10:45:35 +0100

    Updating html element classes

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -12,20 +12,7 @@ $(document).ready(function() {
         var elTarget = document.getElementById("weather-section");
         elTarget.scrollIntoView();
     }
-    // console.log(("#data2").children("img"));
-    //     $("#data2").children("img").click(function() {
-    //         var imgID = $(this).attr("id");
-    //         console.log(imgID);
-    //         var galModal = document.getElementById("galModal");
-    //         // galModal.innerHTML = "";
-    //         galModal.innerHTML = `
-    //         <div id="myModal" class="modal">
-    //         <span class="close">&times;</span>
-    //         <img class="modal-content" id="${imgID}">`;
-    //     });
 
-    //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
-    //code from https://stackoverflow.com/questions/18622508/bootstrap-3-and-youtube-in-modal
     function autoPlayYouTubeModal() {
         var trigger = $("body").find('[data-toggle="modal"]');
         trigger.click(function() {
