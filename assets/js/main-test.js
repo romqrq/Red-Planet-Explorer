a203f043cbfb3da49d360c04a28f5abebf2769c2
romqrq <rjaalbuquerque@gmail.com>
2019-10-04 17:43:01 +0100

    Updated sols and cameras for each rover

--- assets/js/main-test.js
+++ assets/js/main-test.js
@@ -1,53 +1,53 @@
 //Variables for parameters search
-var roverName = ""; //"curiosity";
-var solNumber = ""; //"&sol=1000";
-var camName = ""; //"&camera=FHAZ";
-var pageNumber = ""; //"&page=1"
-var earthDate = ""; //"earth_date=2015-6-3"
+// var roverName = ""; //"curiosity";
+// var solNumber = ""; //"&sol=1000";
+// var camName = ""; //"&camera=FHAZ";
+// var pageNumber = ""; //"&page=1"
+// var earthDate = ""; //"earth_date=2015-6-3"
 
-function getParamsData(cb) {
-    var rvn = document.getElementById("inputRoverName");
-    var roverName = rvn.options[rvn.selectedIndex].value;
-
-    var snb = document.getElementById("solTextInput");
-    var solNumber = `&sol=${snb.value}`;
-    // var snb = document.getElementById("inputSolNumber");
-    // var solNumber = `&sol=${snb.options[snb.selectedIndex].value}`;
-
-    var cmn = document.getElementById("inputCamName");
-    var camName = `&camera=${cmn.options[cmn.selectedIndex].value}`;
-
-    // var pgn = document.getElementById("inputPageNumber");
-    // var pageNumber = `&page=${pgn.options[pgn.selectedIndex].value}`;
-
-    // var ead = document.getElementById("inputEarthDate");
-    // var earthDate = `earth_date=${ead.options[ead.selectedIndex].value}`;
+function getRangeData(roverName, cb) {
+    // var rvn = document.getElementById("inputRoverName");
+    // var roverName = rvn.options[rvn.selectedIndex].value;
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // document.getElementById("data").innerHTML = JSON.parse(this.responseText);
-            console.log(JSON.parse(this.responseText));
             cb(JSON.parse(this.responseText));
-            console.log(cb);
         }
     };
     xhr.open(
         "GET",
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?${earthDate}${solNumber}${camName}${pageNumber}&api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b`
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?&sol=1600&api_key=unJZiQapXhyZamSl37P8FEh7Zlssi7xmaIF4l95b`
     );
+    console.log(roverName);
     xhr.send();
 }
 
-function writeParamsToDocument() {
-    var el = document.getElementById("data2");
-    el.innerHTML = "";
+function writeRangeToDocument() {
+    var rvn = document.getElementById("inputRoverName");
+    var roverName = rvn.options[rvn.selectedIndex].value;
+    console.log(roverName);
 
-    getParamsData(function(data) {
+    getRangeData(roverName, function(data) {
+        console.log(data);
         data = data.photos;
+        var minDate = data[0].rover.landing_date;
+        var maxDate = data[0].rover.max_date;
+        var maxSol = data[0].rover.max_sol;
+        var cn = data[0].rover.cameras;
 
-        data.forEach(function(item) {
-            el.innerHTML += `<img src=${item.img_src} id="${item.id}" class="img-thumbnail" onclick="openModal(this.id, this.src)" data-toggle="modal" data-target="#galleryModal">`;
+        var camNamesList = document.getElementById("inputCamName");
+        camNamesList.innerHTML = "";
+        cn.forEach(function(camera) {
+            // camList.push(camera.name);
+            console.log(camera.name);
+            camNamesList.innerHTML += `
+            <option value="${camera.name}">${camera.name}</option>
+            `;
         });
+
+        var maxSolField = document.getElementById("solTextInput");
+        maxSolField.placeholder = maxSol;
+        console.log(maxSol);
     });
 }
