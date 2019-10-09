8c0a5b651b2909ffb15c1a0d7cb7c9e4fd1bb929
romqrq <rjaalbuquerque@gmail.com>
2019-10-09 18:24:27 +0100

    Added comments and tidied up

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -2,6 +2,35 @@ $(document).ready(function() {
     autoPlayYouTubeModal();
     switchPages();
 
+
+    //Launch button animation
+    $("#launch-button").click(function() {
+        $("header").addClass("shake-animation");
+        $("#launch-button").removeClass("launch-button-animation");
+        setTimeout(scrolldown, 2000);
+    });
+
+    //Function to scroll down to weather after "launch"
+    function scrolldown() {
+        var elTarget = document.getElementById("weather-section");
+        elTarget.scrollIntoView();
+    }
+
+    //function to autoplay videos
+    function autoPlayYouTubeModal() {
+        var trigger = $("body").find('[data-toggle="modal"]');
+        trigger.click(function() {
+            var theModal = $(this).data("target"),
+                videoSRC = $(this).attr("data-theVideo"),
+                videoSRCauto = videoSRC + "?autoplay=1";
+            $(theModal + " iframe").attr("src", videoSRCauto);
+            $(theModal + " button.close").click(function() {
+                $(theModal + " iframe").attr("src", videoSRC);
+            });
+        });
+    }
+
+    //Function for the weight calculator
     $("#weightCheck").click(function() {
         var weight = $("#weightInput").val();
         var testValue = $.isNumeric(weight);
@@ -27,31 +56,6 @@ $(document).ready(function() {
             $("#weightResult").html(`<h4>On Mars, that weight would be equivalent to ${weightOnMars}${unit}!</h4>`)
         };
     });
-
-
-    $("#launch-button").click(function() {
-        $("header").addClass("shake-animation");
-        $("#launch-button").removeClass("launch-button-animation");
-        setTimeout(scrolldown, 2000);
-    });
-
-    function scrolldown() {
-        var elTarget = document.getElementById("weather-section");
-        elTarget.scrollIntoView();
-    }
-
-    function autoPlayYouTubeModal() {
-        var trigger = $("body").find('[data-toggle="modal"]');
-        trigger.click(function() {
-            var theModal = $(this).data("target"),
-                videoSRC = $(this).attr("data-theVideo"),
-                videoSRCauto = videoSRC + "?autoplay=1";
-            $(theModal + " iframe").attr("src", videoSRCauto);
-            $(theModal + " button.close").click(function() {
-                $(theModal + " iframe").attr("src", videoSRC);
-            });
-        });
-    }
 
     //Function to switch the pages for tablets and desktop
     function switchPages() {
